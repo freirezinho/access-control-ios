31f89cabe67141939efb419ede049ac6f842bb08
Saulo Freire <saulo.freire@petzpartner.com.br>
2023-08-31 19:11:35 -0300

    New project

---
+++ Keyz/Keyz/KeyzApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyzApp.swift
+//  Keyz
+//
+//  Created by Saulo Freire on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct KeyzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
